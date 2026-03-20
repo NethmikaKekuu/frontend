8d5d923a748d37032103c37a20eefb57c94b3b1e
IT23657496NethmikaKeku <it23657496@my.sliit.lk>
2026-03-20 19:55:44 +0530

    Frontend initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://localhost:5192/weatherforecast") // change later to Azure URL
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather Forecast</h1>
+
+      {data.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        <table border="1" style={{ margin: "auto" }}>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Temperature (C)</th>
+              <th>Summary</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td>{item.date}</td>
+                <td>{item.temperatureC}</td>
+                <td>{item.summary}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
